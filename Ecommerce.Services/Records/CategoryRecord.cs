0c8965a4ddbad1ce483290a8692796842c4f8429
s-arnav <arnav2294@proton.me>
2025-02-09 01:39:01 +0530

    fixed GA warnings

--- Ecommerce.Services/Records/CategoryRecord.cs
+++ Ecommerce.Services/Records/CategoryRecord.cs
@@ -2,6 +2,6 @@ namespace Ecommerce.Services.Records;
 
 public class CategoryRecord : BaseRecord
 {
-    public string name { get; set; }
-    public string description { get; set; }
+    public string name { get; set; } = "";
+    public string description { get; set; } = "";
 }
--- Ecommerce.Services/Records/ProductRecord.cs
+++ Ecommerce.Services/Records/ProductRecord.cs
@@ -2,8 +2,8 @@ namespace Ecommerce.Services.Records;
 
 public class ProductRecord : BaseRecord
 {
-    public string name { get; set; }
-    public string description { get; set; }
+    public string name { get; set; } = "";
+    public string description { get; set; } = "";
     public decimal price { get; set; }
     public int quantity { get; set; }
     public Guid category_id { get; set; }
